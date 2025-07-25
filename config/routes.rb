28360faac582b7778d7d4609d9fc0f40ea9d1763
anais giovannangeli <giovannangeli.anais@gmail.com>
2025-07-25 18:34:11 +0200

    homepage done

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  root to: "analyses#new"
+  root 'home#index'  # Homepage
   resources :analyses, only: [:index, :new, :create, :show]
 
-  # Pour la vérification de santé de l'app
   get "up" => "rails/health#show", as: :rails_health_check
 end
