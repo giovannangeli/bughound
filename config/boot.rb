0cc445d2a88a4395b249f5665ee724d3930f20f6
anais giovannangeli <giovannangeli.anais@gmail.com>
2025-10-11 20:03:01 +0200

    charge dotenv uniquement en dev et test

--- config/boot.rb
+++ config/boot.rb
@@ -1,5 +1,10 @@
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
-require "dotenv/load"
 require "bundler/setup" # Set up gems listed in the Gemfile.
+
+# Charge dotenv uniquement en environnement de développement et de test
+if ['development', 'test'].include? ENV['RAILS_ENV']
+  require 'dotenv/load'
+end
+
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
