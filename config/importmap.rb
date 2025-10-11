22c19f8183489a24dba314cc4ef6611378d4b958
anais giovannangeli <giovannangeli.anais@gmail.com>
2025-10-11 23:08:04 +0200

    fix importmap

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,19 @@
-# Pin npm packages by running ./bin/importmap
+# config/importmap.rb
 
 pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin_all_from "app/javascript/controllers", under: "controllers"
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+
+# Pin each controller individually from the precompiled assets
+pin "controllers/application", to: "controllers/application.js", preload: true
+pin "controllers/carousel_controller", to: "controllers/carousel_controller.js", preload: true
+pin "controllers/dropdown_controller", to: "controllers/dropdown_controller.js", preload: true
+pin "controllers/file_upload_controller", to: "controllers/file_upload_controller.js", preload: true
+pin "controllers/hello_controller", to: "controllers/hello_controller.js", preload: true
+pin "controllers/mobile_menu_controller", to: "controllers/mobile_menu_controller.js", preload: true
+pin "controllers/modal_controller", to: "controllers/modal_controller.js", preload: true
+pin "controllers/navbar_controller", to: "controllers/navbar_controller.js", preload: true
+pin "controllers/show_controller", to: "controllers/show_controller.js", preload: true
+pin "controllers/user_menu_controller", to: "controllers/user_menu_controller.js", preload: true
+pin "controllers/index", to: "controllers/index.js", preload: true
